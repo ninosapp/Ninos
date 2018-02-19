87b2db0b24bd9574fed6b7fd29406bdb6ba5e9dc
Sumanth <meesalasumanth1@gmail.com>
2018-02-19 14:41:33 +0530

    Quiz view activity added

---
+++ app/src/main/java/com/ninos/activities/QuizViewActivity.java
@@ -0,0 +1,14 @@
+package com.ninos.activities;
+
+import android.os.Bundle;
+
+import com.ninos.R;
+
+public class QuizViewActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_view);
+    }
+}
